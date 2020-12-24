f51e74c78c370eddb23a652fbde9f1c404b71179
au hue nhu <auhuenhu2015@gmail.com>
2020-12-24 23:40:06 +0700

    Nhu up chuc nang dangnhap, dangky, dangxuat

---
+++ dangxuat.php
@@ -0,0 +1,20 @@
+<?php 
+session_start();
+if(isset($_POST['btnDangXuat']))
+{
+
+	if (isset($_SESSION['user']))
+	{
+   }
+   if (isset($_SESSION['ad']))
+	{
+   }
+
+}
+header('location:index.php');
+
+?>
+
+
+
+
